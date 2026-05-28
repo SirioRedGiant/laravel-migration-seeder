e2092bf1113e9de763eaf5cb4ec3f0dc7f2deda8
SirioRedGiant <Oiris1992@gmail.com>
2026-05-28 11:01:59 +0200

    create TrainController to manage the logic, connect that to the index route on home views

---
+++ app/Http/Controllers/TrainController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Train;
+use Illuminate\Http\Request;
+
+class TrainController extends Controller
+{
+    public function index()
+    {
+        // il controller chiede al Model i treni in partenza da oggi in poi, li ordinerà in maniera ascendente cronologicamente
+        $trains = Train::whereDate('departure_time', '>=', today())
+            ->orderby('departure_time', 'asc')
+            ->get();
+
+        dd($trains);
+
+        return view('home', compact('trains'));
+    }
+}
